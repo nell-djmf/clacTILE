588772f8fe83dfa90ba4dbb5bd230533934dbb53
Nell Flores <nell.djmf@gmail.com>
2022-03-28 16:28:18 -0400

    tweaked CSS for homepage, added title and tagline

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './components/Homepage.jsx'
 
-function App() {
+const App = () => {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        {/* Import Nav here */}
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={ <Homepage /> } />
+        </Routes>
+      </main>
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ client/src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+
+  return (
+    <div className="home">
+      <div className='title'>
+        <h1>clacTILE</h1>
+        <h2>
+          Mechanical Keyboard Builder
+        </h2>
+      </div>
+    </div>
+  )
+}
+
+export default Home
