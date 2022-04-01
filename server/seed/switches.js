2d0ed1cdda42d30ffe1673b9e7be11d8e49e083b
Nell Flores <nell.djmf@gmail.com>
2022-03-31 22:08:25 -0400

    switch pictures now rendering

--- server/seed/switches.js
+++ server/seed/switches.js
@@ -9,55 +9,64 @@ const main = async () => {
       name: 'CherryMX Red',
       type: 'Linear',
       actuation: '45g',
-      color: 'Red'
+      color: 'Red',
+      image: 'https://i.imgur.com/xOqQMBD.jpg'
     },
     {
       name: 'Gateron Yellow',
       type: 'Linear',
       actuation: '50g',
-      color: 'Yellow'
+      color: 'Yellow',
+      image: 'https://i.imgur.com/svPcE4v.jpg'
     },
     {
       name: 'Zeal Tealios V2',
       type: 'Linear',
       actuation: '67g',
-      color: 'Teal'
+      color: 'Teal',
+      image: 'https://i.imgur.com/YRL0Dft.jpg'
     },
     {
       name: 'CherryMX Brown',
       type: 'Tactile',
       actuation: '55g',
-      color: 'Brown'
+      color: 'Brown',
+      image: 'https://i.imgur.com/hSNU0Tt.jpg'
     },
     {
       name: 'Glorious Panda',
       type: 'Tactile',
       actuation: '67g',
-      color: 'Orange'
+      color: 'Orange',
+      image: 'https://i.imgur.com/1v2Xxts.jpg'
     },
     {
       name: 'Halo Clear',
       type: 'Tactile',
       actuation: '65g',
-      color: 'White'
+      color: 'White',
+      image: 'https://i.imgur.com/d7KQsLg.jpg'
     },
     {
       name: 'CherryMX Blue',
       type: 'Clicky',
       actuation: '60g',
-      color: 'Blue'
+      color: 'Blue',
+      image: 'https://i.imgur.com/xmuRyip.jpg'
     },
     {
       name: 'Kailh BOX Jade',
       type: 'Clicky',
       actuation: '50g',
-      color: 'Green'
+      color: 'Green',
+      image: 'https://i.imgur.com/EjgYS8j.jpg'
     },
     {
       name: 'NovelKeys x Kailh Sherbet',
       type: 'Clicky',
       actuation: '45g',
-      color: 'Orange'
+      color: 'Orange',
+      image: 'https://i.imgur.com/Nrmcf95.jpg'
     },
   ]
 
